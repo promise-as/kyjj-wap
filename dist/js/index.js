61ce2fc952d3a9e8f4388df38a83195ce1c4d5ac
Hong Baojin <13809735212@163.com>
2019-09-09 18:36:04 +0800

    first commit

---
+++ dist/js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+$(function () {
+  var mySwiper = new Swiper('.swiper-container', {
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev'
+  });
+});
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+$(function () {
+  var mySwiper = new Swiper ('.swiper-container', {
+    // 如果需要前进后退按钮
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+  });
+});
